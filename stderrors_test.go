1f9fadb61ee8af3013beac4fe3b5da707d2ca4d7
George Fekete <george@primalskill.com>
2022-06-25 12:29:34 +0300

    Added tests to std errors Is

---
+++ stderrors_test.go
@@ -0,0 +1,22 @@
+package errors
+
+import (
+	"testing"
+)
+
+func TestStdErrors(t *testing.T) {
+	t.Run("it should have the same error signature", isError)
+}
+
+func isError(t *testing.T) {
+	var validationErr = E("validation error test string", WithMeta("field1", "required"))
+
+	err1 := E("err1 error", validationErr)
+	err2 := E("err2 error", err1)
+
+	if !Is(err2, validationErr) {
+		t.Fatalf("Errors should contain validationErr, got: %+v", err2)
+	}
+}
+
+
