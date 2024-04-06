90904d785d5ada4a2abb92fb8b6ae7a236e1b2c9
George Fekete <george@primalskill.com>
2024-04-06 16:24:50 +0300

    Refactored formatting

--- formatting.go
+++ formatting.go
@@ -2,119 +2,104 @@ package errors
 
 import (
 	"fmt"
-	"strings"
 )
 
 // Error returns the error message and satisfies the stdlib Error interface.
 func (e *Error) Error() string {
+	if len(e.Msg) == 0 {
+		return "<empty>"
+	}
+
 	return e.Msg
 }
 
-// PrettyPrint is a helper method to *Error.PrettyPrint. This should be used in development.
+// PrettyPrint is a helper method to *Error.PrettyPrint. This should only be used in development.
 func PrettyPrint(err error) string {
 	var e *Error
 	is := As(err, &e)
 
 	if is == false {
-		return ""
+		return err.Error()
 	}
 
 	return e.PrettyPrint()
 }
 
 // PrettyPrint will recursively print all embedded errors including all information on the error it can found.
-// This should be used in development.
+// This should only be used in development.
 func (e *Error) PrettyPrint() string {
-	var s strings.Builder
 	err := Flatten(e)
 
-	s.WriteString("\n")
+	var b []byte
+
+	b = append(b, '\n')
 
 	for i, elem := range err {
 		if len(e.Msg) == 0 {
-			s.WriteString("[empty]")
+			b = append(b, "<empty>"...)
 		} else {
-			s.WriteString(elem.Msg)
+			b = append(b, elem.Msg...)
 		}
 
-		s.WriteString(elem.Source.sourcePrettyString(true))
-		s.WriteString(elem.Meta.metaPrettyString(true))
+		b = append(b, elem.Source.sourcePrettyString()...)
+		b = append(b, elem.Meta.metaPrettyString()...)
 
 		if i < len(err) {
-			s.WriteString("\n")
+			b = append(b, '\n')
 		}
-
 	}
 
-	return s.String()
+	return string(b)
 }
 
 // String returns Meta in [key1:val1 key2:val2 ...] format and satisfies the fmt.Stringer interface.
 func (p Meta) String() string {
-	var ret strings.Builder
+	var b []byte
 
-	ret.WriteString("[")
+	b = append(b, '[')
 
 	i := 0
 	for k, v := range p {
-		ret.WriteString(k)
-		ret.WriteString(":")
-
-		str, can := v.(string)
-		if can == true {
-			ret.WriteString(str)
+		str, ok := v.(string)
+		if ok == true {
+			b = fmt.Appendf(b, "%s:%s", k, str)
 		}
 
 		i++
 
 		if i < len(p) {
-			ret.WriteString(" ")
+			b = append(b, ' ')
 		}
 	}
 
-	ret.WriteString("]")
+	b = append(b, ']')
 
-	return ret.String()
+	return string(b)
 }
 
-func (p *Source) sourcePrettyString(isSub bool) string {
+func (p *Source) sourcePrettyString() string {
 	if len(string(*p)) == 0 {
 		return ""
 	}
 
-	padding := ""
-	pipe := ""
+	var b []byte
+	b = fmt.Appendf(b, "\n%*s|- Source : %s", 2, " ", string(*p))
 
-	if isSub {
-		padding = "  "
-		pipe = "|- "
-	}
-
-	return fmt.Sprintf("\n%s%sSource: %s", padding, pipe, string(*p))
+	return string(b)
 }
 
-func (p Meta) metaPrettyString(isSub bool) string {
+func (p Meta) metaPrettyString() string {
 	if len(p) == 0 {
 		return ""
 	}
 
-	padding := ""
-	pipe := ""
-	subPadding := "  "
-	subPipe := "|- "
+	var b []byte
 
-	if isSub {
-		padding = "  "
-		pipe = "|- "
-	}
-
-	var s strings.Builder
-	s.WriteString(fmt.Sprintf("\n%s%sMeta:", padding, pipe))
+	b = fmt.Appendf(b, "\n%*s|- Meta :", 2, " ")
 
 	for k := range p {
-		s.WriteString(fmt.Sprintf("\n%s%s%s", padding, subPadding, subPipe))
-		s.WriteString(fmt.Sprintf("%s: %+v", k, p[k]))
+		b = fmt.Appendf(b, "\n%*s|- %s : %+v", 4, " ", k, p[k])
 	}
 
-	return s.String()
+	return string(b)
 }
